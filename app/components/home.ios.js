f9f846396db02d74837acb18014c6ce4242077a9
hujingliang <1617451312@qq.com>
2016-10-17 13:17:53 +0800

    'IOS工具'

---
+++ app/components/home.ios.js
@@ -0,0 +1,16 @@
+/**
+ * Created by jialao on 2016/10/17.
+ */
+import React, { Component } from 'react';
+import {
+    Component,
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    ListView,
+    Image,
+    Dimensions,
+    ActivityIndicatorIOS
+} from 'react-native';
+
